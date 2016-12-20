ceb191d8b945812533eaf9cb760fc211b1bbc02d
struct78 <david@struct.com.au>
2016-12-21 07:56:37 +1100

    Added comments

--- arduino/src/vector.ino
+++ arduino/src/vector.ino
@@ -1,9 +1,14 @@
+/**
+ * @name dist
+ * @description Gets the distance between two vectors. This is used for the radial pattern.
+ * @param {float} x1 - The x coordinate of the first vector.
+ * @param {float} y2 - The y coordinate of the first vector.
+ * @param {float} x2 - The x coordinate of the second vector.
+ * @param {float} y2 - The y coordinate of the second vector.
+ * @return {float}
+**/
 float dist( float x1, float y1, float x2, float y2 ) {
 		float dx = x1 - x2;
 		float dy = y1 - y2;
 		return (float) sqrt(dx*dx + dy*dy);
-}
-
-float scale( float x, float y ) {
-	return (float) x * y;
 }
--- arduino/src/wifi.ino
+++ arduino/src/wifi.ino
@@ -3,23 +3,27 @@ WebServer webserver(PREFIX, 80);
 
 int wifi_status = WL_IDLE_STATUS;
 
+/**
+ * @name wifi_setup
+ * @description Connects to the Wi-Fi and sets up API routes.
+ * @return {void}
+**/
 void wifi_setup() {
-
-	// check for the presence of the shield:
-	if (WiFi.status() == WL_NO_SHIELD) {
+	// Check for the presence of the shield:
+	if ( WiFi.status() == WL_NO_SHIELD ) {
 		Serial.println("WiFi shield not present");
 		while(true);
 	}
 
-	// attempt to connect to Wifi network:
-	while ( wifi_status != WL_CONNECTED) {
+	// Attempt to connect to Wifi network:
+	while ( wifi_status != WL_CONNECTED ) {
 		Serial.print("Attempting to connect to SSID: ");
 		Serial.println(ssid);
 		wifi_status = WiFi.begin(ssid, pass);
-
-		delay(10000);
+		delay(5000);
 	}
 
+	// Setup API HTTP routes
 	webserver.setDefaultCommand(&routes_default);
 	webserver.addCommand("api/status", &routes_api_status);
 	webserver.addCommand("api/power", &routes_api_power);
@@ -29,27 +33,24 @@ void wifi_setup() {
 	webserver.addCommand("api/pattern", &routes_api_pattern);
 	webserver.begin();
 
-	print_wifi_status();
-	update_dns( WiFi.localIP() );
-}
-
-void print_wifi_status() {
-	Serial.print("SSID: ");
-	Serial.println(WiFi.SSID());
-
+	// The local IP address
 	IPAddress ip = WiFi.localIP();
 	Serial.print("IP Address: ");
 	Serial.println(ip);
 
-	long rssi = WiFi.RSSI();
-	Serial.print("signal strength (RSSI):");
-	Serial.print(rssi);
-	Serial.println(" dBm");
+	// Update the dynamic DNS
+	update_dns( ip );
 }
 
+/**
+ * @name wifi_loop
+ * @description Listens for HTTP connections.
+ * @return {void}
+**/
 void wifi_loop() {
 	char buff[64];
 	int len = 64;
 
+	// Process any connections
 	webserver.processConnection(buff, &len);
 }
