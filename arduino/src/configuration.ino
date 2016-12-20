04aae5e361f6540fbce6cd084f94bfbc65b505b8
struct78 <david@struct.com.au>
2016-12-20 20:55:16 +1100

    Organised variables

--- arduino/src/configuration.ino
+++ arduino/src/configuration.ino
@@ -5,12 +5,16 @@ enum Pattern {
 	PATTERNMAX
 };
 
-bool power          = true;
+/* Neomatrix Specific constants */
+const int cols      = 8;
+const int rows      = 5;
+const int pin       = 6;
+
+/* User-configurable variables */
 float theta         = 0.01;
+bool power          = true;
 int contrast        = 5;
 int brightness      = 40;
-int cols            = 8;
-int rows            = 5;
-int pin             = 6;
-float delta         = 0;
 Pattern pattern     = RADIAL;
+
+float delta         = 0;
