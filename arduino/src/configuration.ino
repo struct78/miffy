349292fa31aa140f7fca13a0c3d7455f00fe2c31
struct78 <david@struct.com.au>
2017-01-05 08:14:27 +1100

    Changed defaults

--- arduino/src/configuration.ino
+++ arduino/src/configuration.ino
@@ -11,10 +11,10 @@ const int rows      = 5;
 const int pin       = 6;
 
 /* User-configurable variables */
-float theta         = 0.01;
+float theta         = 0.5;
 bool power          = true;
-int contrast        = 5;
-int brightness      = 40;
+int contrast        = 2;
+int brightness      = 64;
 Pattern pattern     = RADIAL;
 
 float delta         = 0;
