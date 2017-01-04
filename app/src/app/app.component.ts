6691501d4f3b098f1da6909f5deceb7b5988b0ac
struct78 <david@struct.com.au>
2017-01-05 08:18:15 +1100

    Reconfigured root page

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
-import { HomePage } from '../pages/home/home';
+import { SettingsPage } from '../pages/settings/settings';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class Miffy {
-  rootPage = HomePage;
+  rootPage = SettingsPage;
   constructor( platform: Platform ) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
